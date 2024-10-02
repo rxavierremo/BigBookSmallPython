01d82d96bf1236b3d96968ec7eb0536f6f9618c7
Ranielo Xavier Remo <ranieloremo@gmail.com>
2024-10-03 05:54:47 +0800

    Added bagels.py, first python code

---
+++ assets/bagels.py
@@ -0,0 +1,9 @@
+"""Bagels, by Al Sweigart al@inventpython.com
+A deductive logic game where you must guess a number based on clues.
+View this code at https://nostarch.com/big-book-small-python-projects
+A version of this game is feature in this book "Invent Your Own
+Computer Games with Python" https://nostarch.com/inventwithpython
+Tags: short, game, puzzle"""
+
+import random
+
